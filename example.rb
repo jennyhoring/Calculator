ebefc221d125a174e462c246c6f13334825a3555
jennyhoring <jehoring@gmail.com>
2014-07-10 13:47:32 -0400

    added more practice

---
+++ example.rb
@@ -0,0 +1,12 @@
+def answer_phone(caller, receiver)
+	puts "Hello" + caller + "this is " + receiver
+end
+
+answer_phone(" Max," ,"Jenny")
+
+
+def color(favorite_color)
+	puts "my favorite color is" + favorite_color
+end
+
+color(" green")
